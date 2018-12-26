9049c1d92f0e424ad18e6c3888162f3937f4b319
Sotatek-TinDuong <tin.duong@sotatek.com>
2018-12-27 00:40:50 +0700

    create input component

---
+++ src/Components/InputText.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class InputText extends Component {
+	render() {
+		return (
+			<div className="InputText">
+				<h3>Hello World</h3>
+				<input type="text" name="" placeholder="Input character here" />
+			</div>
+		);
+	}
+}
+
+export default InputText;
