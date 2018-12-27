afd403c326ed4dc61f817b2d186ff2168e922f61
Sotatek-TinDuong <tin.duong@sotatek.com>
2018-12-27 18:02:11 +0700

    load data

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-
+import Data from '../Json/Status';
 class HomePage extends Component {
   render() {
+    // console.log(Data);
     return (
       <div className="HomePage">
         <h4>Hello, This is HomePage</h4>
+        <ul id="listItem">
+        {
+          Data.map(function(item, i){
+            return <li key={i}>{item.status.content}</li>
+          })
+        }
+        </ul>
       </div>
     );
   }
---
+++ src/Json/Status.js
@@ -0,0 +1,19 @@
+const Data = [
+  {
+      "status": {
+        "content": "Hello, my name is Herry",
+      },
+  },
+  {
+      "status": {
+        "content": "I'm reading comics",
+      },
+  },
+  {
+      "status": {
+        "content": "Do something",
+      },
+  }
+];
+
+export default Data;
